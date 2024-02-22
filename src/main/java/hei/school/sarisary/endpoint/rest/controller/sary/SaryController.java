6b16b2f05924ceffd2c28d03aa03db605d158b43
Zo-Tsilavina <hei.tsilavina@gmail.com>
2024-02-22 12:19:36 +0300

    feat: add fonctionnality to converte image to black and white

---
+++ src/main/java/hei/school/sarisary/endpoint/rest/controller/sary/SaryController.java
@@ -0,0 +1,28 @@
+package hei.school.sarisary.endpoint.rest.controller.sary;
+
+import java.io.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import hei.school.sarisary.service.sary.SaryService;
+@RestController
+public class SaryController {
+
+  @Autowired private SaryService saryService;
+
+  @RequestMapping(
+      method = RequestMethod.PUT,
+      path = "/blacks/{id}",
+      consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+  public String turnToBlackAndWhite(
+      @PathVariable(name = "id") String id, @RequestPart MultipartFile file) throws IOException {
+    System.out.println(file);
+    return saryService.uploadTransformedImage(id, file);
+  }
+
+  @GetMapping("/blacks/{id}")
+  public String getBlackAndWhiteImage(@PathVariable String id) {
+    return id + "is blacked";
+  }
+}
---
+++ src/main/java/hei/school/sarisary/repository/model/OperationSary.java
@@ -0,0 +1,16 @@
+package hei.school.sarisary.repository.model;
+
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OperationSary {
+  @Id private String id;
+
+  @Column(name = "transformation_timestamp")
+  private Timestamp operationTimestamp;
+}
